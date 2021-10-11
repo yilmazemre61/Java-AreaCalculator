9fb64a1977f10070f08a41e591b8ba9ae5474850
Emre <61523720+yilmazemre61@users.noreply.github.com>
2021-10-11 15:56:00 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        AreaCalculator.area(5.0);
+        AreaCalculator.area(-1);
+        AreaCalculator.area(5.0, 4.0);
+        AreaCalculator.area(-1.0,4.0);
+    }
+}
